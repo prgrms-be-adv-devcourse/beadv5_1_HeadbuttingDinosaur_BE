ef768bb51f618847f0f5b0e7caf453a2a23f13bd
bjb991123 <bjb991123@naver.com>
2026-03-24 09:25:14 +0900

    chore(event): OpenAPI 설정 추가

---
+++ event/src/main/java/com/devticket/event/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.devticket.event.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI eventOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("DevTicket Event API")
+                .description("이벤트 서비스 API 문서")
+                .version("v1.0.0"));
+    }
+}
