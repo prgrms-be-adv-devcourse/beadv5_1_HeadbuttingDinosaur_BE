a2ac319a8c3c81bad30e3423e3088f1965abcba0
HyWChoi <chw2309@icloud.com>
2026-03-25 17:49:04 +0900

    fix(member): 서비스 작성 전까지 잠시 테스트 비활성화

--- member/src/test/java/com/devticket/member/MemberApplicationTests.java
+++ member/src/test/java/com/devticket/member/MemberApplicationTests.java
@@ -1,11 +1,9 @@
 package com.devticket.member;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 
-@Profile("test")
-@SpringBootTest
+//@Profile("test")
+//@SpringBootTest
 class MemberApplicationTests {
 
     @Test
