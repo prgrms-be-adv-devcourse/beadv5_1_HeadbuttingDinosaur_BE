ecdd953d0f1860b5b9b028f7eb9186c709ffa2df
HyWChoi <chw2309@icloud.com>
2026-03-25 16:13:42 +0900

    hotfix(member): 로컬 db kafka 연결 포트 변경,테스트 진입점에 Profile 어노테이션 추가

--- member/src/test/java/com/devticket/member/MemberApplicationTests.java
+++ member/src/test/java/com/devticket/member/MemberApplicationTests.java
@@ -2,12 +2,14 @@ package com.devticket.member;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
 class MemberApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
