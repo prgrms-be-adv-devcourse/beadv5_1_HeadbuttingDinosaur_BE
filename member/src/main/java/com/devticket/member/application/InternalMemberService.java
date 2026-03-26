1d02802b37fb902a0cb8a729ce55692b575ea516
HyWChoi <chw2309@icloud.com>
2026-03-26 16:40:26 +0900

    feat(member): Internal API 컨트롤러 및 Service 뼈대 생성
    - InternalMemberController 생성 (getMemberInfo, getMemberStatus, getMemberRole, getSellerInfo)
    - InternalMemberService 빈 껍데기 생성
    - Swagger 어노테이션 적용

---
+++ member/src/main/java/com/devticket/member/application/InternalMemberService.java
@@ -0,0 +1,31 @@
+package com.devticket.member.application;
+
+import com.devticket.member.presentation.dto.internal.response.InternalMemberInfoResponse;
+import com.devticket.member.presentation.dto.internal.response.InternalMemberRoleResponse;
+import com.devticket.member.presentation.dto.internal.response.InternalMemberStatusResponse;
+import com.devticket.member.presentation.dto.internal.response.InternalSellerInfoResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InternalMemberService {
+
+    public InternalMemberInfoResponse getMemberInfo(Long userId) {
+        // TODO: Phase 4에서 구현
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public InternalMemberStatusResponse getMemberStatus(Long userId) {
+        // TODO: Phase 4에서 구현
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public InternalMemberRoleResponse getMemberRole(Long userId) {
+        // TODO: Phase 4에서 구현
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public InternalSellerInfoResponse getSellerInfo(Long userId) {
+        // TODO: Phase 4에서 구현
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
