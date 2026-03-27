33e04c059c6eecae68272f63b838ada01032466a
bona3 <bona373737@gmail.com>
2026-03-27 10:36:35 +0900

    fix(commerce) : ci테스트환경설정

--- commerce/src/test/java/com/devticket/commerce/CommerceApplicationTests.java
+++ commerce/src/test/java/com/devticket/commerce/CommerceApplicationTests.java
@@ -2,12 +2,14 @@ package com.devticket.commerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
