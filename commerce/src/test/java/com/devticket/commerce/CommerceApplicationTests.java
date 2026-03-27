c809eead1ab697d17c4653db452aca974a8bf9b2
bona3 <bona373737@gmail.com>
2026-03-27 10:52:52 +0900

    fix(commerce) : test환경에 external설정값

--- commerce/src/test/java/com/devticket/commerce/CommerceApplicationTests.java
+++ commerce/src/test/java/com/devticket/commerce/CommerceApplicationTests.java
@@ -1,11 +1,16 @@
 package com.devticket.commerce;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {
+    KafkaAutoConfiguration.class
+})
 class CommerceApplicationTests {
 
     @Test
