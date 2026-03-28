7a2e0141e1e28bc4971337215f20ca363e393165
bona3 <bona373737@gmail.com>
2026-03-28 16:03:36 +0900

    fix(commerce) : 엔티티에 getter어노테이션 추가, 소계합산 calcSubtotalAmount함수 추가

--- commerce/src/main/java/com/devticket/commerce/order/domain/model/Order.java
+++ commerce/src/main/java/com/devticket/commerce/order/domain/model/Order.java
@@ -19,11 +19,13 @@ import java.util.List;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @SuperBuilder
+@Getter
 @Table(name = "order", schema = "commerce")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
