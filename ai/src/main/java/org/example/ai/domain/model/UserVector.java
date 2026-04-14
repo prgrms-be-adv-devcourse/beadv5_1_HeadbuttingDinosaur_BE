877932e275d79c65826e43b990065a00683f4829
Kangila404 <ia3264666@gmail.com>
2026-04-14 15:08:04 +0900

    fix(ai) : ai user-vector .json mapping으로 변환에 따른 userVector, initializer 생성

--- ai/src/main/java/org/example/ai/domain/model/UserVector.java
+++ ai/src/main/java/org/example/ai/domain/model/UserVector.java
@@ -11,46 +11,37 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Getter
 @Builder
-@Document(indexName = "user-index")
+@Document(indexName = "user-index", createIndex = false)
 public class UserVector {
 
     @Id
     private String id;
 
     // ============ 장기 취향 벡터 ============ //
-    @Field(type = FieldType.Dense_Vector)
     private float[] preferenceVector;
 
-    @Field(type = FieldType.Float)
     private float preferenceWeightSum;
     // ===================================== //
 
     // ============ 구매 의도 벡터 ============ //
-    @Field(type = FieldType.Dense_Vector)
     private float[] cartVector;
 
-    @Field(type = FieldType.Float)
     private float cartWeightSum;
     // ===================================== //
 
     // ============ 최근 관심 벡터 ============ //
-    @Field(type = FieldType.Dense_Vector)
     private float[] recentVector;
 
-    @Field(type = FieldType.Float)
     private float recentWeightSum;
     // ===================================== //
 
     // ============ 부정 신호 vector ============ //
-    @Field(type = FieldType.Dense_Vector)
     private float[] negativeVector;
 
-    @Field(type = FieldType.Float)
     private float negativeWeightSum;
     // ===================================== //
 
     // vector 갱신 시각
-    @Field(type = FieldType.Date)
     private String updatedAt;
 
 
