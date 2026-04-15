03ee11c2e74b8b96d9f233d5e888b0513edc0793
chw2309@icloud.com <chw2309@icloud.com>
2026-04-15 12:40:36 +0900

    fix(settlement): 에러 수정 - SettlementController : "/api" 리퀘스트 매핑 추가 - getSellerSettlements 빈 객체 응답 시 에러가 아닌 빈 객체 그대로 반환

--- settlement/src/main/java/com/devticket/settlement/presentation/controller/SettlementController.java
+++ settlement/src/main/java/com/devticket/settlement/presentation/controller/SettlementController.java
@@ -15,11 +15,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Settlement", description = "정산 외부 API")
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class SettlementController {
 
