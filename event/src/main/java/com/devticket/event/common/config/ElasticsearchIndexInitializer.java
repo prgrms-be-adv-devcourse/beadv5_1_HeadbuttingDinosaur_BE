14dc9a8dd7eab9b326a0ae1f95f06741e96d6b73
bjb991123 <bjb991123@naver.com>
2026-04-21 16:52:30 +0900

    refactor(event): ci 통과를 위한 @Profile("!test") 추가

--- event/src/main/java/com/devticket/event/common/config/ElasticsearchIndexInitializer.java
+++ event/src/main/java/com/devticket/event/common/config/ElasticsearchIndexInitializer.java
@@ -9,12 +9,14 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class ElasticsearchIndexInitializer {
 
