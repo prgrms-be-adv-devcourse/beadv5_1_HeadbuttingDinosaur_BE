48b57cb93456463c355dc10ada18bf746351c2c3
Kangila404 <ia3264666@gmail.com>
2026-04-27 12:31:12 +0900

    fix(event) : EventControllerTest mock 추가

--- event/src/test/java/com/devticket/event/presentation/controller/EventControllerTest.java
+++ event/src/test/java/com/devticket/event/presentation/controller/EventControllerTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.devticket.event.application.EventRecommendationService;
 import com.devticket.event.application.EventService;
 import com.devticket.event.domain.enums.EventCategory;
 import com.devticket.event.domain.enums.EventStatus;
@@ -51,6 +52,9 @@ class EventControllerTest {
 
     @MockitoBean
     private EventService eventService;
+
+    @MockitoBean
+    private EventRecommendationService eventRecommendationService;
 
     // 이벤트 생성 테스트
 
